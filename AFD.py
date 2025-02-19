995e8604da875a9d7486fa45af14bf3351f19a2d
Angela Rosana Garcia <100092195+angelargd8@users.noreply.github.com>
2025-02-19 00:26:15 -0600

    feat: finished construir_AFD, add buscar_raiz, construir_tabla_transicion & definir_estados_aceptacion

--- AFD.py
+++ AFD.py
@@ -30,17 +30,86 @@ class AFD:
             for simbolo in self.transiciones[estado_origen]:
                 estado_destino = self.transiciones[estado_origen][simbolo]
                 print(f"{estado_origen} -> {estado_destino} [simbolo: {simbolo}]")
-        print("---"*7)
+        print("---"*15)
 
     def __str__(self):
         return f"AFD(estados={self.estados}, alfabeto={self.alfabeto}, estado_inicial={self.estado_inicial}, estados_finales={self.estados_finales}, transiciones={self.transiciones})"
 
 
+def buscar_raiz(followpos):
+    nodos =set(followpos.keys())
+    valores = set() 
 
-def construir_AFD(raiz, followpos):
+    for conjunto in followpos.values():
+        # print(conjunto)
+        valores.update(conjunto)
+
+    raiz = (nodos - valores)
+    # print(f"Nodos: {nodos}")
+    # print(f"Valores: {valores}")
+    # print(f"Diferencia (Raiz): {raiz}")
+    if len(raiz) !=0:
+        raiz = list(raiz)[0]
+    else:
+        raiz = list(nodos)[0] 
+    # print(f"Raiz final: {raiz}")
+    return raiz
+
+
+def construir_tabla_transicion(afd, followpos,alfabeto ):
+    #recorrer los estados y el follow pos
+    for estado, posiciones in followpos.items():
+        for pos in posiciones:
+            afd.transiciones[estado] = {}
+
+        #recorrer el alfabeto
+        for simbolo in alfabeto:    
+            if simbolo not in afd.transiciones[estado]:
+                afd.transiciones[estado][simbolo] = set()
+            afd.transiciones[estado][simbolo].add(pos)
+    return afd.transiciones
+
+def definir_estados_aceptacion(afd,  posicion_aceptacion):
+    for estado in afd.estados:
+        if posicion_aceptacion == estado:
+            afd.agregar_estado_final(estado)
+    return afd.estados_finales
+
+def construir_AFD(arbolSintactico, followpos):
+    #instanciar afd
     afd = AFD()
-    #1. identificar los estados del AFD
-    print(raiz)
+    #1. definir el estado inicial
+    #buscar la raiz del nodo
+    raiz = buscar_raiz(followpos)
+    afd.agregar_estado_inicial(raiz)
+
+    #2. identificar los estados del AFD    
+    #estados
+    for estados in followpos:
+        # print(estados)
+        afd.estados.append(estados)
+
+    #crear el alfabeto recorriendo el arbol
+    nodos = [arbolSintactico]
+    while nodos:
+        nodo = nodos.pop()
+        if (nodo.value.isalnum() and nodo.value!= 'ε') or (nodo.value.isalnum() and nodo.value!= 'e') :
+            afd.alfabeto.add(nodo.value)
+        if nodo.left:
+            nodos.append(nodo.left)
+        if nodo.right:
+            nodos.append(nodo.right)
+   
+    #3. construir la tabla de transicion  con el follow pos
+    construir_tabla_transicion(afd, followpos, afd.alfabeto)
+    
+    #4. definir los estados de aceptacion
+    #identificar la posicion de aceptacion
+    posicion_aceptacion = max(followpos.keys())
+
+    #buscar los estados que tengan la posicion de aceptacion
+    definir_estados_aceptacion(afd, posicion_aceptacion)
+
     return afd
 
 
