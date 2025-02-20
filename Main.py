fa16d1a67a2265886e61519bd51e3808c41da38d
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-02-19 18:35:39 -0600

    feat: graficadora try

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 # Laboratorio - Construcción Directa de AFD y ecosistema de reconocimiento de expresiones regulares
-#autores: - Francis Alguilar, 22243
+# autores: - Francis Alguilar, 22243
 #         - Cesar Lopez, 22505
 #         - Angela Garcia, 22869
-'''
+"""
 El objetivo es desarrollar la base del generador de analizadores léxicos. A partir de r deberá construir un AFD, utilizando el algoritmo de construcción directa.
 
 Objetivos
@@ -21,39 +21,41 @@ Debe aceptar expresiones regulares para definir el AFD y también cadenas para p
 Al procesar las expresiones regulares, muestre en pantalla el AFD.
 Luego permita procesar cadenas, y para cada una indique si es aceptada o no.
 
-'''
+"""
 
 import os
 
-from constructor import leerArchivo, construirArbol,  construirArbolSintactico 
-from regex import  infixToPostfix
+from constructor import leerArchivo, construirArbol, construirArbolSintactico
+from graficadora import graficar_afd
+from regex import infixToPostfix
 from afn import *
 from AFD import *
 from node import *
 from collections import deque, defaultdict
 
 
-
 def construirAFD(postfix):
-    #construir el arbol sintactico para la cadena
+    # construir el arbol sintactico para la cadena
     ArbolSintactico = construirArbolSintactico(postfix)
     # print(ArbolSintactico)
-    #el followpos es la tablita para saber las posiciones que se tienen que seguir
+    # el followpos es la tablita para saber las posiciones que se tienen que seguir
     followpos = defaultdict(set)
     calcular_followPos(ArbolSintactico, followpos)
     # print(followpos)
-    #construir el AFD
+    # construir el AFD
     afd = construir_AFD(ArbolSintactico, followpos)
-    afd.mostrar()
+    # afd.mostrar()
     return afd
 
 
-expresiones = leerArchivo('./expresiones.txt')
+expresiones = leerArchivo("./expresiones.txt")
 
 # Procesar cada expresión regular y generar el AFD
-for expresion in expresiones:
-    print("Expresion ingresada: \n"+expresion)
+for i, expresion in enumerate(expresiones):
+    print("Expresion ingresada: \n" + expresion)
     postfix = infixToPostfix(expresion)
-    print ("Expresion POSTFIX \n"+ postfix + "\n")
-    construirAFD(postfix)
-    
+    print("Expresion POSTFIX \n" + postfix + "\n")
+    afd = construirAFD(postfix)
+
+    nombre_archivo = f"AFD {i} "
+    afd.mostrar()
