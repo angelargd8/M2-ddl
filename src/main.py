9dd836b79258426374fe1d189662f9a162ade868
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-03-22 18:02:50 -0600

    chore: clean code

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Create a file handler and a stream handlerh
-file_handler = logging.FileHandler('afd_generation.log')
+file_handler = logging.FileHandler("afd_generation.log")
 stream_handler = logging.StreamHandler()
 
 # Create a formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
 
@@ -37,7 +37,6 @@ def construirAFD(postfix):
     else:
         print("Error al construir el arbol sintactico")
 
-
     # # # el followpos es la tablita para saber las posiciones que se tienen que seguir
     followpos = defaultdict(set)
     calcular_followPos(ArbolSintactico, followpos)
@@ -52,16 +51,16 @@ expresiones = leerArchivo("../expresiones.txt")
 
 # Procesar cada expresión regular y generar el AFD
 for i, expresion in enumerate(expresiones):
-    logger.info(f'Procesando expresion {i + 1}: {expresion} \n')
+    logger.info(f"Procesando expresion {i + 1}: {expresion} \n")
     postfix = infixToPostfix(expresion)
-    print(f'Expresion POSTFIX: {postfix}\n')
+    print(f"Expresion POSTFIX: {postfix}\n")
 
     afd = construirAFD(postfix)
 
     # afd.mostrar()
-    logger.info('AFD mostrado')
+    logger.info("AFD mostrado")
 
     # Generar la imagen del AFD usando la función de visualización
     # Aquí usamos la expresión regular como parte del nombre, pero se sanitiza internamente.
     visualize_afd(afd, output_dir="Visual_AFD", file_name=f"AFD_{i}")
-    logger.info(f'Imagen del AFD generada: AFD_{i}')
+    logger.info(f"Imagen del AFD generada: AFD_{i}")
