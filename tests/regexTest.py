7bb86e68dd8d761138fc04d9081763926f828780
Angela Rosana Garcia <100092195+angelargd8@users.noreply.github.com>
2025-03-24 00:53:35 -0600

    feat: add regex test

---
+++ tests/regexTest.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from Automata.Regex import *
+
+class TestRegex(unittest.TestCase):
+
+    def test_infixToPostfix(self):
+        self.assertEqual(infixToPostfix("(a|b)*abb"), "ab|*a.b.b.#.")
+        self.assertEqual(infixToPostfix("(a|b)+"), "ab|ab|*.#.")
+        self.assertEqual(infixToPostfix("(\t|\n)+"), "\t\n|\t\n|*.#.")
+        self.assertEqual(infixToPostfix("(a|b)\+"), "ab|\+.#.")
+
+if __name__ == '__main__':
+    unittest.main()
